48edaddce761c8cf8433b6ccc77e898ced57a431
kthompson5 <111643299+kthompson5@users.noreply.github.com>
2025-03-05 20:01:02 -0600

    Add files via upload

---
+++ games.js
@@ -0,0 +1,13 @@
+
+const games = [
+    { game: "Yankees vs Astros" },
+    { game: "Dodgers vs Braves" },
+    { game: "Mets vs Phillies" },
+    { game: "Cubs vs Cardinals" },
+    { game: "Red Sox vs Orioles" },
+    { game: "Giants vs Padres" },
+    { game: "Rangers vs Mariners" },
+    { game: "Brewers vs Pirates" },
+    { game: "Tigers vs Twins" },
+    { game: "Angels vs Athletics" }
+];
---
+++ sound.js
@@ -0,0 +1,5 @@
+
+function playSound() {
+    const audio = new Audio('sounds/bat-crack.mp3');
+    audio.play();
+}
